90005011c0895267e0cef6259ab32105c53be24e
Kevin Wenger <wenger.kev@gmail.com>
2023-10-18 16:47:27 +0200

    cover InlineCommentSpacerFixer

---
+++ tests/Unit/Fixer/InlineCommentSpacerFixerTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Unit\Fixer;
+
+use drupol\PhpCsFixerConfigsDrupal\Fixer\InlineCommentSpacerFixer;
+use drupol\PhpCsFixerConfigsDrupal\Tests\FixerTestCase;
+use PhpCsFixer\Fixer\FixerInterface;
+
+/**
+ * @author Kevin Wenger <wenger.kev@gmail.com>
+ *
+ * @internal
+ *
+ * @covers \drupol\PhpCsFixerConfigsDrupal\Fixer\InlineCommentSpacerFixer
+ */
+final class InlineCommentSpacerFixerTest extends FixerTestCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->fixer = $this->createFixer();
+    }
+
+    protected function createFixer(): FixerInterface
+    {
+        return new InlineCommentSpacerFixer();
+    }
+
+    /**
+     * @dataProvider provideFixCases
+     */
+    public function testFix(string $expected, string $input = null): void
+    {
+        $this->doTest($expected, $input);
+    }
+
+    public static function provideFixCases(): iterable
+    {
+        yield [
+            '<?php // Lorem Ipsum.',
+            '<?php //Lorem Ipsum.',
+        ];
+
+        yield [
+            '<?php // lorem Ipsum.',
+            '<?php //lorem Ipsum.',
+        ];
+
+        yield [
+            '<?php // lorem ipsum',
+            '<?php //lorem ipsum',
+        ];
+    }
+}
